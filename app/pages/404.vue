7b76b47a0a677e10d8b0c00d045527a3424f859a
MAOX <dengye@gmail.com>
2026-05-26 16:15:30 +0800

    Add 404 page with countdown redirect to link

---
+++ app/pages/404.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="container">
+    <h1>该商品的活动已结束</h1>
+    <p>{{ countdownText }}</p>
+  </div>
+</template>
+
+<script setup>
+import { ref, onMounted } from 'vue'
+
+const countdown = ref(3)
+const countdownText = ref('3 秒后自动跳转京东红包领取页面，祝您中大奖！！！')
+
+onMounted(() => {
+  const timer = setInterval(() => {
+    countdown.value--
+    countdownText.value = `${countdown.value} 秒后自动跳转京东红包领取页面，祝您中大奖！！！`
+    
+    if (countdown.value <= 0) {
+      clearInterval(timer)
+      window.location.href = 'https://kzurl10.cn/t2z8oz'
+    }
+  }, 1000)
+})
+</script>
+
+<style scoped>
+.container {
+  width: 100vw;
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  font-size: 18px;
+  color: #333;
+}
+</style>
